64431dacaeb4cfac6cae64a6ec2942635a850b90
Emma Wilcox <icecubesky@gmail.com>
2017-04-19 14:29:32 -0400

    component setup complete, but logic for component communication still missing

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
+
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('name', 4),
+    new Ingredient('name', 5),
+    new Ingredient('name', 1)
+  ];
 
   constructor() { }
 
