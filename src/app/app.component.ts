87503ac2e46faccd16748e096953de8e658d6bce
Emma Wilcox <icecubesky@gmail.com>
2017-05-03 17:58:32 -0400

    databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedPage = 'recipe';
 
+  onNavigate(page: string) {
+    this.loadedPage = page;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
     recipes: Recipe[] = [
-      new Recipe('name', 'desc', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRzYIce0G9YVmDLDRlZkSJ4KBI4GlylfUBlO66ZgBfNu8Ek9dYbeA'),
-      new Recipe('name', 'desc', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRzYIce0G9YVmDLDRlZkSJ4KBI4GlylfUBlO66ZgBfNu8Ek9dYbeA'),
-      new Recipe('name', 'desc', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRzYIce0G9YVmDLDRlZkSJ4KBI4GlylfUBlO66ZgBfNu8Ek9dYbeA')
+      new Recipe('name1', 'desc1', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRzYIce0G9YVmDLDRlZkSJ4KBI4GlylfUBlO66ZgBfNu8Ek9dYbeA'),
+      new Recipe('name2', 'desc2', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRzYIce0G9YVmDLDRlZkSJ4KBI4GlylfUBlO66ZgBfNu8Ek9dYbeA'),
+      new Recipe('name3', 'desc3', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRzYIce0G9YVmDLDRlZkSJ4KBI4GlylfUBlO66ZgBfNu8Ek9dYbeA')
     ];
 
   ngOnInit() {
+  }
+
+  recipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
